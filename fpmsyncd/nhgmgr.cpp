3964b8ea8747129ef6505a2f1de84da4e8dbef5b
Eddie Ruan <eddie.ruan@alibaba-inc.com>
2026-05-16 09:59:08 -0700

    Add debug

--- fpmsyncd/nhgmgr.cpp
+++ fpmsyncd/nhgmgr.cpp
@@ -615,6 +615,8 @@ void RIBNHGTable::delEntry(uint32_t id) {
     if (entry->isSharedSonicNHG()){
         this->subSonicNHGObjectRef(entry->getSonicNHGObjectKey());
     }else{
+        SWSS_LOG_NOTICE("delEntry: remove entry from NHG DB for RIB_id %d, sonic_id %d", 
+                         entry->getRIBID(), entry->getSonicObjID());
         this->removeFromDB(sonicNHGID);
         m_sonic_id_manager->freeID(SONIC_NHG_OBJ_TYPE_NHG_NORMAL, sonicNHGID);
         m_sonic_nhg_id_2_rib_nhg_id_map.erase(sonicNHGID);
@@ -713,6 +715,9 @@ int RIBNHGTable::writeToDB(RIBNHGEntry *entry) {
     }
     entry->setLastAppdbFields(fields_str);
 
+    SWSS_LOG_NOTICE("writeToDB : RIB_id %d sonic_id %d, fields %s", 
+                    entry->getRIBID(), entry->getSonicObjID(), fields_str.c_str())
+
     m_nexthop_groupTable.set(std::to_string(entry->getSonicObjID()), fvVector);
 
     // At present, the specification of NHG is not large.
@@ -771,6 +776,8 @@ void RIBNHGTable::subSonicNHGObjectRef(SonicNHGObjectKey key) {
      * if refCount is 0, then remove the sonic nhg object
      */
     if(m_created_shared_nhg_map[key].refCount == 0){
+         SWSS_LOG_NOTICE("subSonicNHGObjectRef: sonic_id %d",
+                         m_created_shared_nhg_map[key].id);
         this->removeFromDB(m_created_shared_nhg_map[key].id);
         m_sonic_id_manager->freeID(SONIC_NHG_OBJ_TYPE_NHG_NORMAL, m_created_shared_nhg_map[key].id);
         m_created_shared_nhg_map.erase(key);
@@ -1509,7 +1516,9 @@ u_int32_t SonicIDMgr::allocateID(sonicNhgObjType type) {
         SWSS_LOG_ERROR("SonicIDAllocator is not exist: %d", type);
         return 0;
     }
-    return allocator->allocateID();
+    auto id = allocator->allocateID();
+    SWSS_LOG_NOTICE("Allocate id : type %d id %d", (int) type, (int) id);
+    return id;
 }
 
 void SonicIDMgr::freeID(sonicNhgObjType type, uint32_t id) {
@@ -1518,6 +1527,7 @@ void SonicIDMgr::freeID(sonicNhgObjType type, uint32_t id) {
         SWSS_LOG_ERROR("SonicIDAllocator is not exist: %d", type);
         return;
     }
+    SWSS_LOG_NOTICE("Free id : type %d id %d", (int) type, (int) id);
     allocator->freeID(id);
 }
 

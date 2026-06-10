fa69522b0510068e386526c2e89cc3e06f43bd51
Eddie Ruan <eddie.ruan@alibaba-inc.com>
2026-06-10 15:08:37 +0800

    Fix UT: set NEXTHOP_FLAG_ACTIVE in SRv6 VPN test helper

--- tests/mock_tests/fpmsyncd/ut_helpers_fpmsyncd.cpp
+++ tests/mock_tests/fpmsyncd/ut_helpers_fpmsyncd.cpp
@@ -627,7 +627,7 @@ namespace ut_fpmsyncd
         inet_pton(AF_INET6, src_addr, &src_in.ipv6);
         fib::g_addr rmap_src_in;
         std::uint8_t weight_in = 1;
-        std::uint8_t flags_in = 0;
+        std::uint8_t flags_in = 1;
         std::uint32_t nhg_flags_in = 1024;
         bool has_srv6 = true;
         bool has_seg6_segs = true;

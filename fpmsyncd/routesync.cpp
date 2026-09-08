4d46f59ad5883a89831688a20bb03932c354f548
Yuqing Zhao <galadriel.zyq@alibaba-inc.com>
2026-09-08 14:21:44 +0800

    fpmsyncd: import APP_STATE_DB support from sonic-swss#4421
    Squash the APP_STATE_DB integration and corresponding mock-test updates from
    upstream PR #4421 while retaining the current route-performance ZMQ path.
    
    Source: https://github.com/sonic-net/sonic-swss/pull/4421
    Upstream-Head: c69eec9675780b1db4d7da235e98da304cf777e4

--- fpmsyncd/routesync.cpp
+++ fpmsyncd/routesync.cpp
@@ -171,7 +171,7 @@ static decltype(auto) makeNlAddr(const T& ip)
 }
 
 
-RouteSync::RouteSync(RedisPipeline *pipeline) :
+RouteSync::RouteSync(RedisPipeline *pipeline, RedisPipeline *app_state_pipeline) :
     // When route_performance zmq is enabled, route events must be sent to orchagent via the ZMQ channel.
     m_zmqClient(create_route_perf_zmq_client()),
     m_routeTable(createProducerStateTable(pipeline, APP_ROUTE_TABLE_NAME, true, m_zmqClient)),
@@ -186,7 +186,9 @@ RouteSync::RouteSync(RedisPipeline *pipeline) :
     m_warmStartHelper(pipeline, m_routeTable.get(), APP_ROUTE_TABLE_NAME, "bgp", "bgp"),
     m_srv6MySidTable(pipeline, APP_SRV6_MY_SID_TABLE_NAME, true),
     m_srv6SidListTable(pipeline, APP_SRV6_SID_LIST_TABLE_NAME, true),
-    m_nl_sock(NULL), m_link_cache(NULL)
+    m_nl_sock(NULL), m_link_cache(NULL),
+    m_app_state_pipeline(app_state_pipeline),
+    m_nhgFullStateTable(app_state_pipeline, "NHG_FULL_STATE_TABLE", true)
 {
     m_nl_sock = nl_socket_alloc();
     nl_connect(m_nl_sock, NETLINK_ROUTE);
